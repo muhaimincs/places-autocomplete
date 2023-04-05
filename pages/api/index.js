7b8be793e36805c7010cc1c35fcf225126f0cb39
Muhaimin CS <muhaimincs@gmail.com>
2023-04-05 18:15:18 +0800

    update env vars

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.q}&types=establishment&key=AIzaSyBOcxIZzfq5AtzywMF8MwpKyX2AUM6gUvY`)
+  const response = await fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.q}&types=establishment&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}`)
   const data = await response.json()
   res.status(200).json(data)
 }
